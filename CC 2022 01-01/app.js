40aa2443d9b94852431f5d8845da61400db3afde
Colin Borows <colborows@gmail.com>
2022-01-01 11:58:27 -0600

    Add coding challenge for 01-01-2022

---
+++ CC 2022 01-01/app.js
@@ -0,0 +1,29 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+
+
+
+//PREP
+
+//PARAMETERS - will it always be integers? are we going to be given any special characters? an empty array ever?
+
+
+//RETURN - Return a new arr with each value doubled
+
+//EXAMPLES - If we are given [2,3,4], should return [4,6,8]
+// If we are given [4,5,6], should return [8,10,12]
+// If we are given [2,22], should return [4,6,8]
+
+
+//PSEUDOCODE - 
+//Make a function that takes in an array
+
+//map through array and multiply each element by 2 and return
+
+function doubled(arr){
+   return arr.map(e=>e*2)
+}
